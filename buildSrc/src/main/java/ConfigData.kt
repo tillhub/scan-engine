d1a22693a7a0eb035a8fac6f01231829c49b464f
Martin Sirok <m.sirok@gmail.com>
2022-02-17 10:56:00 +0100

    fixed naming issue

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -6,7 +6,7 @@ object ConfigData {
     const val minSdkVersion = 21
     const val targetSdkVersion = 31
     const val versionCode = 1
-    const val versionName = "1.2.0"
+    const val versionName = "1.3.0"
 
     val JAVA_VERSION = JavaVersion.VERSION_11
     val JVM_TARGET = JavaVersion.VERSION_11.toString()
