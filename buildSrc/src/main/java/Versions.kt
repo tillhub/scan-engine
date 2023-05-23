b6c34274cb728cdbc7759fe6e3c8bed472f33d1a
Đorđe Hrnjez <djordjehrnjez@gmail.com>
2023-05-23 14:22:09 +0200

    UNTIL-6709 Libs update

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -9,27 +9,25 @@ object Versions {
     }
 
     object Kotlin {
-        const val COROUTINES = "1.6.0"
+        const val COROUTINES = "1.7.1"
     }
 
     object AndroidX {
-        const val CORE_KTX = "1.7.0"
-        const val APP_COMPAT = "1.3.1"
-
-        const val CONSTRAINT_LAYOUT = "2.1.1"
-
-        const val FRAGMENT = "1.4.0"
+        const val CORE_KTX = "1.10.0"
+        const val APP_COMPAT = "1.6.1"
+        const val CONSTRAINT_LAYOUT = "2.1.4"
+        const val FRAGMENT = "1.5.7"
     }
 
     object Google {
-        const val MATERIAL = "1.4.0"
-        const val HILT = "2.38.1"
+        const val MATERIAL = "1.7.0"
+        const val HILT = "2.45"
         const val MLKIT = "17.0.0"
     }
 
     object CameraX {
-        const val CAMERA_X = "1.1.0-beta01"
-        const val CAMERA_VIEW = "1.1.0-beta01"
+        const val CAMERA_X = "1.2.2"
+        const val CAMERA_VIEW = "1.2.2"
     }
 
     object Testing {
