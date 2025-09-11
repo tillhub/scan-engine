d2355e03a331ce92ff0bac2c836cc42772311683
Đorđe Hrnjez <djordjehrnjez@gmail.com>
2025-09-11 10:35:57 +0200

    Commons tests migration

--- scan-engine/src/commonTest/kotlin/de/tillhub/scanengine/ui/components/BottomButtonTest.kt
+++ scan-engine/src/commonTest/kotlin/de/tillhub/scanengine/ui/components/BottomButtonTest.kt
@@ -1,30 +1,26 @@
-package de.tillhub.scanengine.ui
+@file:OptIn(ExperimentalTestApi::class)
+
+package de.tillhub.scanengine.ui.components
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import de.tillhub.scanengine.ui.components.BottomButton
+import androidx.compose.ui.test.runComposeUiTest
 import de.tillhub.scanengine.ui.theme.AppTheme
-import org.junit.Assert.assertEquals
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
+import kotlin.test.DefaultAsserter.assertEquals
+import kotlin.test.Test
 
-@RunWith(AndroidJUnit4::class)
-class BottomButtonInstrumentedTest {
-    @get:Rule
-    val composeTestRule = createComposeRule()
+class BottomButtonTest {
 
     @Test
-    fun submitButtonTest() {
+    fun submitButtonTest() = runComposeUiTest {
         var clicks = 0
 
-        composeTestRule.setContent {
+        setContent {
             AppTheme {
                 BottomButton(
                     isEnable = true,
@@ -37,8 +33,8 @@ class BottomButtonInstrumentedTest {
             }
         }
 
-        composeTestRule.onNodeWithTag("submitButton").performClick()
-        composeTestRule.onNodeWithContentDescription("Bottom button label").assertTextEquals("Submit")
+        onNodeWithTag("submitButton").performClick()
+        onNodeWithContentDescription("Bottom button label").assertTextEquals("Submit")
 
         assertEquals("onClick should be called once", 1, clicks)
     }
