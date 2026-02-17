8f75e64bcfaa1aa65c1501a51f2580d9ff729713
Martin <m.sirok@gmail.com>
2026-02-17 12:57:38 +0100

    added legacy contract to work with old android implementation

---
+++ scan-engine/src/androidMain/kotlin/de/tillhub/scanengine/camera/contract/legacy/CameraScanContract.kt
@@ -0,0 +1,36 @@
+package de.tillhub.scanengine.camera.contract.legacy
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import de.tillhub.scanengine.camera.ui.CameraScanActivity
+import de.tillhub.scanengine.data.ScannerEvent
+
+class CameraScanContract : ActivityResultContract<String?, ScannerEvent>() {
+    override fun createIntent(
+        context: Context,
+        input: String?
+    ): Intent = Intent(context, CameraScanActivity::class.java).apply {
+        input?.let {
+            putExtra(CameraScanActivity.SCAN_KEY, it)
+        }
+    }
+
+    override fun parseResult(
+        resultCode: Int,
+        intent: Intent?
+    ): ScannerEvent {
+        val resultData = intent?.extras?.getString(CameraScanActivity.DATA_KEY)
+        val scanKey = intent?.extras?.getString(CameraScanActivity.SCAN_KEY)
+
+        return if (resultCode == Activity.RESULT_OK && !resultData.isNullOrEmpty()) {
+            ScannerEvent.ScanResult(
+                value = resultData,
+                scanKey = scanKey,
+            )
+        } else {
+            ScannerEvent.Camera.Canceled
+        }
+    }
+}
